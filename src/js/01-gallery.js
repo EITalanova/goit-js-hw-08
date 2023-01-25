fb6a0f36425629320b0b1cc18914a78e3b3baac2
EITalanova <elenabelichenko77@gmail.com>
2023-01-24 16:46:53 -0800

    Initial commit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,21 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+// // console.log(galleryItems);
+
+// const gallery = document.querySelector(".gallery");
+
+// const markupGallery = galleryItems
+//   .map(
+//     (galleryItem) =>
+//       `<a class="gallery__item" href="${galleryItem.original}">
+//   <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+// </a>`
+//   )
+//     .join("");
+  
+// gallery.insertAdjacentHTML('beforeend', markupGallery);
+
+// var lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: "250" });
---
+++ src/js/02-video.js
@@ -0,0 +1,9 @@
+
+// import Player from '@vimeo/player';
+
+// const iframe = document.querySelector('#vimeo-player');
+// const iframePlayer = new Vimeo.Player(iframe);
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
