747c708b20c0cdae9b2f66ce075e8eae3e31629f
EITalanova <elenabelichenko77@gmail.com>
2023-01-25 13:25:13 -0800

    Task 1 and 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-// import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-// const iframe = document.querySelector('#vimeo-player');
-// const iframePlayer = new Vimeo.Player(iframe);
+const KEY = 'videoplayer-current-time';
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+player.on('timeupdate', throttle(data => {
+    localStorage.setItem(KEY, JSON.stringify(data));
+  }, 1000)
+);
+
+const savedTimePauseStr = localStorage.getItem(KEY);
+const savedTimePauseOb = JSON.parse(savedTimePauseStr);
+
+player
+  .setCurrentTime(savedTimePauseOb.seconds)
+  .then(function (savedTimePauseOb) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,25 @@
 
+const form = document.querySelector('.feedback-form');
+const dataSet = {
+    email: document.querySelector('input[name="email"]'),
+    message: document.querySelector('textarea[name="message"]'),
+};
+
+const KEY_STORAGE = "feedback-form-state";
+// console.log(email);
+// console.log(email.value);
+
+
+function v(evt) {
+    const emailValue = evt.target.value;
+    // const messageValue = evt.target.value;
+    // console.log(emailValue);
+    // console.log(messageValue);
+
+localStorage.setItem(KEY_STORAGE, JSON.stringify(emailValue));
+
+};
+
+
+form.addEventListener('input', v);
+// dataSet.addEventListener('input', v);
