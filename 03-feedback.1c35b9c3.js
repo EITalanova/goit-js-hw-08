1dcefb5d3451de2d52cf501ace6aa52659ecf6bf
EITalanova <98708004+EITalanova@users.noreply.github.com>
2023-01-25 11:26:21 +0000

    Deploying to gh-pages from @ EITalanova/goit-js-hw-08@747c708b20c0cdae9b2f66ce075e8eae3e31629f 🚀

---
+++ 03-feedback.1c35b9c3.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");document.querySelector('input[name="email"]'),document.querySelector('textarea[name="message"]');e.addEventListener("input",(function(e){const t=e.target.value;localStorage.setItem("feedback-form-state",JSON.stringify(t))}));
+//# sourceMappingURL=03-feedback.1c35b9c3.js.map
---
+++ 03-feedback.3750fec0.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");document.querySelector('input[name="email"]'),document.querySelector('textarea[name="message"]');e.addEventListener("input",(function(e){var t=e.target.value;localStorage.setItem("feedback-form-state",JSON.stringify(t))}))}();
+//# sourceMappingURL=03-feedback.3750fec0.js.map
